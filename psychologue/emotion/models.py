9e463d16d976c747ec3223f2614b495c07b2fda9
MaximeR12 <rowell.maxime2001@gmail.com>
2023-07-11 13:56:12 +0200

    first page of django

--- psychologue/emotion/models.py
+++ psychologue/emotion/models.py
@@ -1,19 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Psychologist(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-
-class Patient(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-
-
 class Text(models.Model):
-    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    psy_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    patient_id = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    emotion = models.CharField(max_length=100)  # Placeholder, replace with your emotion model's output
+    emotion = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
--- psychologue/psychologue/urls.py
+++ psychologue/psychologue/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from emotion import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", views.new_text, name="new_text"),
 ]
