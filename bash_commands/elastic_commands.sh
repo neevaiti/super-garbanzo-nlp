142828416c0c5b7dcaa43659981bf296f1dc3838
Ne'evaiti <52328785+neevaiti@users.noreply.github.com>
2023-07-12 23:28:56 +0200

    build: add bash_commands directory

---
+++ bash_commands/elastic_commands.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# obtenir la liste des index d'une connexion
+curl -X GET "http://localhost:9200/_cat/indices?v"
+
+# Health : Indique l'état de santé du cluster Elasticsearch par rapport à l'index. Les valeurs possibles sont green (vert), yellow (jaune) et red (rouge). green signifie que tous les nœuds de données contiennent toutes les copies de l'index, yellow signifie que toutes les copies primaires sont disponibles, mais certaines copies répliques ne le sont pas, et red signifie que certaines données sont indisponibles.
+# Status : Indique l'état d'ouverture de l'index. open signifie que l'index est ouvert et accessible pour les opérations de lecture/écriture.
+# UUID : L'identifiant unique de l'index.
+# Primary shards : Le nombre de shards primaires utilisés pour stocker l'index.
+# Replica shards : Le nombre de shards répliques utilisés pour stocker l'index.
+# docs.count : Le nombre total de documents dans l'index.
+# docs.deleted : Le nombre de documents supprimés dans l'index.
+# store.size : La taille totale du stockage utilisé par l'index, y compris les données primaires et les répliques.
+# pri.store.size : La taille du stockage primaire utilisé par l'index, qui exclut les répliques.
