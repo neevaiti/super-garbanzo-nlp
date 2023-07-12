8b287f94fc85f7f74a13fcc5b88b86d2dd34f3d9
Ne'evaiti <52328785+neevaiti@users.noreply.github.com>
2023-07-12 23:30:16 +0200

    feat: modify urls pattern

--- psychologue/psychologue/urls.py
+++ psychologue/psychologue/urls.py
@@ -1,23 +1,14 @@
-"""psychologue URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from emotion import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.new_text, name="new_text"),
+    path('', views.homepage, name='homepage'),
+    path("new-text/", views.new_text, name="new_text"),
+    path('patient/<int:id>/', views.text_by_id, name='text_by_id'),
+    # path("research/", views.research, name="research"),
+    path('login-user/', views.login_user, name='login'),
+    path('logout-user/', views.logout_user, name='logout'),
+    path('register-user/', views.register_user, name='register')
 ]
